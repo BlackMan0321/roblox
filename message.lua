c5034081714f3f27c31e653c0f08471749502c4e
BlackMan <108860016+BlackMan0321@users.noreply.github.com>
2025-05-17 19:36:06 +0300

    Create message.lua

---
+++ message.lua
@@ -0,0 +1,9 @@
+local MessageLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/BlackMan0321/roblox/main/message")))()
+ MessageLib:MakeNotification(
+      {
+          Name = "LBMScriptz | Roblox",
+          Content = "Hile Aktif Edildi",
+          Image = "rbxassetid://122200870759804",
+          Time = 5
+      }
+  )
